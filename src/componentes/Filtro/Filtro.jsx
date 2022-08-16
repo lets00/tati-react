ec86b8750019e698123dfae2f09a0bcffeca1fac
LUIS EDUARDO TENORIO SILVA <1044473@LABS.GARANHUNS.IFPE>
2022-08-16 16:21:13 -0300

    Adicionado component Filtro e Titulo

---
+++ src/componentes/Filtro/Filtro.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Filtro() {
+  return (
+    <div className="row">
+        <div className="col-12 col-lg-10">
+            <input/>
+        </div>
+        <div className="col-12 col-lg-1">
+            <strong>Países: 999</strong>
+        </div>
+        <div className="col-12 col-lg-1">
+            <strong>População: 9.999.999.999</strong>
+        </div>
+    </div>
+  )
+}
+
+export default Filtro
